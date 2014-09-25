755e4080a877cba2dcbf7cf98a1037b3e2ff649c
Michael Flynn <mflynn37211@yahoo.com>
2014-09-25 16:45:08 -0400

    Changed more Hound errors

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -16,7 +16,8 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  #   # ActionController::Base.helpers.asset_path("fallback/"
+  # + [version_name, "default.png"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
@@ -36,7 +37,7 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   #   %w(jpg jpeg gif png)
   # end
   # Override the filename of the uploaded files:
-  # Avoid using model.id or version_name here, see uploader/store.rb for details.
+  # Avoid using model.id or version_name here, see uploader/store.rb for details
   # def filename
   #   "something.jpg" if original_filename
   # end
