4717047fce677448b085656eb2977554493b3954
louis hoang <bawsing@gmail.com>
2014-09-29 14:24:01 -0400

    added another query for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,7 @@
 genre_url = 'http://api.trakt.tv/genres/shows.json/b6cb98c70268917b494f3ed68fd73720'
 response = HTTParty.get(genre_url)
 genres = JSON.parse(response.body)
+many_shows = []
 
 i = 0
 id = 1
@@ -19,12 +20,17 @@ genres.each do |genre|
   genre_and_genre_id[genre["name"]] = genre["id"]
 end
 
-show_url = 'http://api.trakt.tv/search/shows.json/b6cb98c70268917b494f3ed68fd73720?query=star'
+show_url = 'http://api.trakt.tv/search/shows.json/b6cb98c70268917b494f3ed68fd73720?query=the'
 response = HTTParty.get(show_url)
-shows = JSON.parse(response.body)
+many_shows << JSON.parse(response.body)
 
-shows.each do |show|
-  Show.find_or_create_by(name: show["title"], description: show["overview"], genre_id: genre_and_genre_id[show["genres"].first], year: show["year"])
+show_url = 'http://api.trakt.tv/search/shows.json/df51bd1d01acd1352732bca955244eda?query=star'
+response = HTTParty.get(show_url)
+many_shows << JSON.parse(response.body)
+many_shows.each do |shows|
+  shows.each do |show|
+    Show.find_or_create_by(name: show["title"], description: show["overview"], genre_id: genre_and_genre_id[show["genres"].first], year: show["year"])
+  end
 end
 
 # User.create(username: "hah3", profile_photo: "string_url_of_photo")
