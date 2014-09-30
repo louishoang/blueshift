ec621ab207e8bc2411b2854a5f754f9840142ca5
louis hoang <bawsing@gmail.com>
2014-09-30 11:19:42 -0400

    added link to profile page validates uniqueness for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+  validates :username, uniqueness: true
   validates :username, presence: true, length: { in: (6..32) }
   has_many :reviews
   has_many :votes
