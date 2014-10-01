55c8104fc4eb81c3f2d740d7408c7d4824eb165f
David Sok <sokd88@gmail.com>
2014-10-01 11:51:44 -0400

    added capybara testing for search

--- spec/features/user_searches_show_spec.rb
+++ spec/features/user_searches_show_spec.rb
@@ -6,13 +6,13 @@ feature "User searches for a show" do
 
     user = FactoryGirl.create(:user)
     show = FactoryGirl.create(:show)
+
     sign_in_as(user)
 
-    fill_in "Search Shows", with: show.name
-    save_and_open_page
-    click_on "Search"
+    fill_in 'search', with: show.name
+    click_button "Search"
 
-    expect(page).to have_content "Show"
+    expect(page).to have_content show.name
 
   end
 
