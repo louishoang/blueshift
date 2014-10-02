57a335ab6b29647f486bbc5826413a2a9834313e
louis hoang <bawsing@gmail.com>
2014-10-02 18:28:49 -0400

    fixed the css on showpage.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,5 +16,11 @@
 //= require turbolinks
 //= require owl.carousel
 //= require_tree .
-$(function(){ $(document).foundation(); });
+$(function(){ $(document).foundation({
+  equalizer : {
+      // Specify if Equalizer should make elements equal height once they become stacked.
+      equalize_on_stack: true
+    }
+  });
+});
 
